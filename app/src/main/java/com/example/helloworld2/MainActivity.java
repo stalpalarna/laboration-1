a4a853b4894d4c46502d28ca8ea25abe65d4bf7e
Filip <fipe2200@student.miun.se>
2025-01-24 14:38:44 +0100

    Test commit 5

--- app/src/main/java/com/example/helloworld2/MainActivity.java
+++ app/src/main/java/com/example/helloworld2/MainActivity.java
@@ -19,7 +19,7 @@ import android.view.Menu;
 import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
-    //Test commit 4
+    //Test commit 5
     private AppBarConfiguration appBarConfiguration;
     private ActivityMainBinding binding;
 
